ebe1518e9ed7b89ae25d93e2a40483d1893824c5
AbdulHakeemKzm <abdulhakeemdev@gmail.com>
2023-12-03 13:36:00 +0530

    simple project2

--- basic projects/Python-Mysql_connector/python.py
+++ basic projects/Python-Mysql_connector/python.py
@@ -1,11 +1,12 @@
 import mysql.connector
 
 mysqlconnector = mysql.connector.connect(
-user = "ardit700_student",
-password = "ardit700_student",
-host = "108.167.140.122",
-database = "ardit700_pm1databasse"
+user = "uname",
+password = "password",
+host = "IP",
+database = "DBname"
 )
+
 
 cursor = con.cursor()
 
